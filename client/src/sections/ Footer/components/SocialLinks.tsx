192e38613823e7c04723c4e210cc95810693eb67
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 20:55:31 +0700

    Create SocialLinks.tsx

---
+++ client/src/sections/ Footer/components/SocialLinks.tsx
@@ -0,0 +1,58 @@
+export const SocialLinks = () => {
+  return (
+    <div className="items-center box-border caret-transparent flex flex-col mt-8 md:items-start md:flex-row">
+      <div className="box-border caret-transparent flex">
+        <a
+          href="#"
+          className="text-2xl box-border caret-transparent block leading-9 mr-2 mt-3 md:mr-6 hover:text-violet-500"
+        >
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-17.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </a>
+        <a
+          href="#"
+          className="text-2xl box-border caret-transparent block leading-9 mr-2 mt-3 md:mr-6 hover:text-violet-500"
+        >
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-18.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </a>
+        <a
+          href="#"
+          className="text-2xl box-border caret-transparent block leading-9 mr-2 mt-3 md:mr-6 hover:text-violet-500"
+        >
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-19.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </a>
+        <a
+          href="#"
+          className="text-2xl box-border caret-transparent block leading-9 mr-2 mt-3 md:mr-6 hover:text-violet-500"
+        >
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-20.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </a>
+        <a
+          href="#"
+          className="text-2xl box-border caret-transparent block leading-9 mr-2 mt-3 md:mr-6 hover:text-violet-500"
+        >
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-21.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
