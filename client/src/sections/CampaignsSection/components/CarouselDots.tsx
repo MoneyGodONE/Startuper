9cd9a3b44f011ed1bf503ead5f09982786658cdc
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 20:48:02 +0700

    Create CarouselDots.tsx

---
+++ client/src/sections/CampaignsSection/components/CarouselDots.tsx
@@ -0,0 +1,12 @@
+export const CarouselDots = () => {
+  return (
+    <div className="box-border caret-transparent flex justify-center mt-3.5">
+      <div className="bg-gray-800 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+      <div className="bg-gray-400 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+      <div className="bg-gray-400 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+      <div className="bg-gray-400 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+      <div className="bg-gray-400 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+      <div className="bg-gray-400 box-border caret-transparent h-2.5 w-2.5 mx-[5px] rounded-full"></div>
+    </div>
+  );
+};
