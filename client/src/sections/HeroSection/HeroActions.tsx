85fecb3738dc0baf7c6219e7b028a3b4ef86ef44
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:01:08 +0700

    Create HeroActions.tsx

---
+++ client/src/sections/HeroSection/HeroActions.tsx
@@ -0,0 +1,20 @@
+export const HeroActions = () => {
+  return (
+    <div className="absolute box-border caret-transparent gap-x-1 flex gap-y-1 right-4 bottom-4">
+      <button className="text-2xl bg-zinc-900/50 caret-transparent block leading-9 text-center p-2 rounded-full">
+        <img
+          src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-10.svg"
+          alt="Icon"
+          className="box-border caret-transparent h-6 w-6"
+        />
+      </button>
+      <button className="text-2xl bg-zinc-900/50 caret-transparent block leading-9 text-center p-2 rounded-full">
+        <img
+          src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-11.svg"
+          alt="Icon"
+          className="box-border caret-transparent h-6 w-6"
+        />
+      </button>
+    </div>
+  );
+};
