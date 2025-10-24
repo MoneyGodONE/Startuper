5c82568165a00005dd86afe5e9544e1525b303fd
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:05:15 +0700

    Create index.tsx

---
+++ client/src/sections/ InfoSection/index.tsx
@@ -0,0 +1,13 @@
+export const InfoSection = () => {
+  return (
+    <div className="box-border caret-transparent max-w-none text-center w-full mx-auto pt-24 pb-10 md:max-w-screen-xl">
+      <h1 className="text-3xl font-semibold box-border caret-transparent leading-9 mb-4 md:text-4xl md:leading-10">
+        Find it first on Block Fund.
+      </h1>
+      <p className="box-border caret-transparent">
+        Block Fund is where early adopters and innovation seekers find lively,
+        imaginative tech before it hits the mainstream.
+      </p>
+    </div>
+  );
+};
