727ba21c86ff12767afc26cf6955ed104f244bd9
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:06:09 +0700

    Create SidebarIcon.tsx

---
+++ client/src/sections/Sidebar/components/SidebarIcon.tsx
@@ -0,0 +1,11 @@
+export const SidebarIcon = () => {
+  return (
+    <div className="items-center bg-zinc-900 box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+      <img
+        src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-5.svg"
+        alt="Icon"
+        className="box-border caret-transparent h-5 w-5"
+      />
+    </div>
+  );
+};
