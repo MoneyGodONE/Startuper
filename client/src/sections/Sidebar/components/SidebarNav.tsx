f32ac261c4aab91b79bad46e87c04a0520e0c60f
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:07:07 +0700

    Create SidebarNav.tsx

---
+++ client/src/sections/Sidebar/components/SidebarNav.tsx
@@ -0,0 +1,43 @@
+export const SidebarNav = () => {
+  return (
+    <div className="items-center bg-zinc-900 box-border caret-transparent flex basis-[0%] flex-col grow justify-between min-h-0 min-w-0 w-[76px] mt-12 py-4 rounded-[20px] md:min-h-[auto] md:min-w-[auto]">
+      <div className="items-center box-border caret-transparent gap-x-3 flex flex-col justify-center min-h-0 min-w-0 gap-y-3 md:min-h-[auto] md:min-w-[auto]">
+        <div className="items-center bg-zinc-800 box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-1.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </div>
+        <div className="items-center box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-2.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </div>
+        <div className="items-center box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-3.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </div>
+        <div className="items-center box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+          <img
+            src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-4.svg"
+            alt="Icon"
+            className="box-border caret-transparent h-6 w-6"
+          />
+        </div>
+      </div>
+      <div className="items-center bg-zinc-900 box-border caret-transparent flex h-12 justify-center min-h-0 min-w-0 w-12 rounded-[10px] md:min-h-[auto] md:min-w-[auto]">
+        <img
+          src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-5.svg"
+          alt="Icon"
+          className="box-border caret-transparent h-5 w-5"
+        />
+      </div>
+    </div>
+  );
+};
