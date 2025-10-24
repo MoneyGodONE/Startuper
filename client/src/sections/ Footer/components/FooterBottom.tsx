21b80040db0f139aa27e8e711c013259015a0a0a
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 20:53:41 +0700

    Create FooterBottom.tsx

---
+++ client/src/sections/ Footer/components/FooterBottom.tsx
@@ -0,0 +1,36 @@
+export const FooterBottom = () => {
+  return (
+    <div className="text-sm box-border caret-transparent leading-5 text-center">
+      <ul className="box-border caret-transparent flex flex-col flex-nowrap justify-center list-none pl-0 md:flex-row md:flex-wrap">
+        <li className="box-border caret-transparent mr-4 mb-2 md:mb-0">
+          <a href="#" className="box-border caret-transparent">
+            Term of Use
+          </a>
+        </li>
+        <li className="box-border caret-transparent mr-4 mb-2 md:mb-0">
+          <a href="#" className="box-border caret-transparent">
+            Privacy Policy
+          </a>
+        </li>
+        <li className="box-border caret-transparent mr-4 mb-2 md:mb-0">
+          <a href="#" className="box-border caret-transparent">
+            Cookie Policy
+          </a>
+        </li>
+        <li className="box-border caret-transparent mr-4 mb-2 md:mb-0">
+          <a href="#" className="box-border caret-transparent">
+            Do Not Sell My Personal Information
+          </a>
+        </li>
+        <li className="box-border caret-transparent mr-4 mb-2 md:mb-0">
+          <a href="#" className="box-border caret-transparent">
+            Accessibility
+          </a>
+        </li>
+      </ul>
+      <p className="box-border caret-transparent">
+        © 2024 Block Fund. Inc. All Rights Reserved
+      </p>
+    </div>
+  );
+};
