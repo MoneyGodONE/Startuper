6682afca184befca5340f3bf191fbe22bbb6d938
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 20:58:51 +0700

    Create DesktopAuthButton.tsx

---
+++ client/src/sections/Header/components/DesktopAuthButton.tsx
@@ -0,0 +1,12 @@
+export const DesktopAuthButton = () => {
+  return (
+    <div className="items-center box-border caret-transparent gap-x-4 hidden justify-end min-h-0 min-w-0 gap-y-4 md:flex md:min-h-[auto] md:min-w-[auto]">
+      <button
+        type="button"
+        className="font-semibold bg-violet-500 caret-transparent inline-block leading-[26px] min-h-[52px] min-w-0 text-center px-4 py-0 rounded-[10px] font-epilogue md:block md:min-w-[auto]"
+      >
+        Log In / Sign up
+      </button>
+    </div>
+  );
+};
