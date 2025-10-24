44585ee5d081a46083609114f61646ce8882e9ca
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 20:47:20 +0700

    Add CampaignHeader component for campaign display

---
+++ client/src/sections/CampaignsSection/components/CampaignHeader.tsx
@@ -0,0 +1,17 @@
+export const CampaignHeader = () => {
+  return (
+    <div className="box-border caret-transparent flex justify-between">
+      <h1 className="text-lg font-semibold box-border caret-transparent leading-[27px] text-left my-10 font-epilogue">
+        All Campaigns
+      </h1>
+      <h1 className="text-lg font-semibold items-center box-border caret-transparent flex leading-[27px] text-left my-10 font-epilogue">
+        All Campaigns
+        <img
+          src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/icon-12.svg"
+          alt="Icon"
+          className="box-border caret-transparent h-[18px] -rotate-45 w-[18px]"
+        />
+      </h1>
+    </div>
+  );
+};
