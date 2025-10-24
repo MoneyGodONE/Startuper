fa10dfdb6c03d8794ba497b312ea36df6a0ff908
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:06:41 +0700

    Create SidebarLogo.tsx

---
+++ client/src/sections/Sidebar/components/SidebarLogo.tsx
@@ -0,0 +1,14 @@
+export const SidebarLogo = () => {
+  return (
+    <a
+      href="/"
+      className="box-border caret-transparent block min-h-0 min-w-0 md:min-h-[auto] md:min-w-[auto]"
+    >
+      <img
+        src="https://c.animaapp.com/mh4w5bh5ddZvQu/assets/BlockFundLogo.8e4907f5.png"
+        alt="logo"
+        className="aspect-[auto_50_/_50] box-border caret-transparent max-w-full w-[50px] rounded-md"
+      />
+    </a>
+  );
+};
