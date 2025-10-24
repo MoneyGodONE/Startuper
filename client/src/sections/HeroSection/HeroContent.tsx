863ed9cc870a18b43ff8f004857b5f05ef38c5cf
Money God ONE [MGO] <moneygodone@outlook.com>
2025-10-24 21:01:29 +0700

    Create HeroContent.tsx

---
+++ client/src/sections/HeroSection/HeroContent.tsx
@@ -0,0 +1,13 @@
+export const HeroContent = () => {
+  return (
+    <div className="absolute backdrop-blur-lg bg-white/20 box-border caret-transparent p-2 rounded-bl rounded-br rounded-tl rounded-tr bottom-2 inset-x-4 md:left-10 md:bottom-4">
+      <h3 className="text-black text-xl font-bold box-border caret-transparent leading-7 text-start w-[66.6667%] md:text-3xl md:leading-9 md:text-justify">
+        MOTOR-ZERO 2.0:A new definition of a razor{" "}
+      </h3>
+      <p className="text-black text-xl box-border caret-transparent leading-7 text-start w-[66.6667%] md:text-justify">
+        Portable|Mini|self-sharpening Blades|Replaceable Blades|IPX7
+        waterproof|Wit...
+      </p>
+    </div>
+  );
+};
